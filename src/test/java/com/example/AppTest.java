90b517d5cb311a726e6f86f8bd19d3a634c0ebfe
dawniejulian <103433163+dawniejulian@users.noreply.github.com>
2025-06-13 22:36:31 +0700

    percobaan 1 continous testing

--- src/test/java/com/example/AppTest.java
+++ src/test/java/com/example/AppTest.java
@@ -3,7 +3,7 @@ package com.example;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class AppTest {
-    @Test
+    @Test 
     public void testTambah() {
         assertEquals(5, App.tambah(2, 3));
     }
